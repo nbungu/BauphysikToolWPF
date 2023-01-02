897e748a624c31a7c08af34e62d1b335f1f6d79e
Arne@YOGA370 <arne.schunter@gmail.com>
2023-01-02 22:34:42 +0100

    Zwischenstand

--- UI/ViewModels/LiveChartsViewModel.cs
+++ UI/ViewModels/LiveChartsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using LiveChartsCore.SkiaSharpView;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore;
 using System;
 using System.Collections.Generic;
@@ -6,23 +7,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+using LiveChartsCore.SkiaSharpView.Painting.Effects;
 
 namespace BauphysikToolWPF.UI.ViewModels
 {
-    //ViewModel for OxyPlot Graph: Used in xaml as "DataContext"
-    public class LiveChartsViewModel
+    //ViewModel for LiveCharts: Used in xaml as "DataContext"
+    [ObservableObject]
+    public partial class LiveChartsViewModel
     {
-        public ISeries[] Series { get; set; }
+        public ISeries[] TempCurve { get; set; }
+        public RectangularSection[] Layers { get; set; }
+        public Axis[] XAxes { get; set; }
+        public Axis[] YAxes { get; set; }
+        public SolidColorPaint TooltipTextPaint { get; set; }
+        public SolidColorPaint TooltipBackgroundPaint { get; set; }
+        public SolidColorPaint LegendTextPaint { get; set; }
+        public SolidColorPaint LegendBackgroundPaint { get; set; }
 
         public LiveChartsViewModel()
         {
-            this.Series = new ISeries[]
+            this.Layers = new RectangularSection[]
             {
-                new LineSeries<double>
+                new RectangularSection
                 {
-                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
-                    Fill = null
+                    Yi = 5, //TODO: Taupunkttemp
+                    Yj = 5,
+                    Stroke = new SolidColorPaint
+                    {
+                        Color = SKColors.Red,
+                        StrokeThickness = 1,
+                        PathEffect = new DashEffect(new float[] { 2, 2 })
+                    }
+                },
+                new RectangularSection
+                {
+                    Xi = 1,
+                    Xj = 3,
+                    Fill = new SolidColorPaint { Color = SKColors.Blue.WithAlpha(20) }
                 }
+            };
+            this.TempCurve = new ISeries[]
+            {
+                new LineSeries<int>
+                {
+                    Values = new int[] { 4, 6, 5, 3, -3 },
+                    Fill = null,
+                    GeometrySize = 3, // size of the connecting dots                   
+                    LineSmoothness = 0, // where 0 is a straight line and 1 the most curved
+                    Stroke = new LinearGradientPaint(new[]{ new SKColor(45, 64, 89), new SKColor(255, 212, 96)}) { StrokeThickness = 3 },
+                    GeometryStroke = new LinearGradientPaint(new[]{ new SKColor(45, 64, 89), new SKColor(255, 212, 96)}) { StrokeThickness = 3 }
+                }
+            };
+            this.XAxes = new Axis[]
+            {
+                new Axis
+                {
+                    Name = "X Axis",
+                    //Labels = new string[] { "Layer 1", "Layer 2", "Layer 3", "Layer 4", "Layer 5" },
+                    NamePaint = new SolidColorPaint(SKColors.Black),
+
+                    LabelsPaint = new SolidColorPaint(SKColors.Black),
+                    TextSize = 14,
+
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightGray) { StrokeThickness = 1 }
+                }
+            };
+            this.YAxes = new Axis[]
+{
+                new Axis
+                {
+                    Name = "Y Axis",
+                    NamePaint = new SolidColorPaint(SKColors.Black),
+
+                    LabelsPaint = new SolidColorPaint(SKColors.Black),
+                    TextSize = 14,
+
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightGray)
+                    {
+                        StrokeThickness = 1,
+                        PathEffect = new DashEffect(new float[] { 3, 3 })
+                    }
+                }
+            };
+            this.TooltipTextPaint = new SolidColorPaint
+            {
+                Color = new SKColor(0, 0, 0),
+                SKTypeface = SKTypeface.FromFamilyName("Courier New")
+            };
+            this.TooltipBackgroundPaint = new SolidColorPaint(new SKColor(255, 255, 255));
+
+            this.LegendBackgroundPaint = new SolidColorPaint(new SKColor(240, 240, 240));
+
+            this.LegendTextPaint = new SolidColorPaint
+            {
+                Color = new SKColor(50, 50, 50),
+                SKTypeface = SKTypeface.FromFamilyName("Courier New")
             };
         }
     }
