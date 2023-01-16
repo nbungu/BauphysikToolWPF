b993fe0be484542e1e0705569346c0a9c20b3cb1
arne.schunter <arne.schunter@gmail.com>
2023-01-16 09:36:12 +0100

    Hatching pattern fertig. Anpassung noch nötig

--- UI/DrawLayerCanvas.cs
+++ UI/DrawLayerCanvas.cs
@@ -71,7 +71,7 @@ namespace BauphysikToolWPF.UI
                     Height = canvasHeight,
                     Stroke = layer.IsSelected ? Brushes.Blue : Brushes.Black,
                     StrokeThickness = layer.IsSelected ? 1 : 0.4,
-                    Fill = Test3()//new SolidColorBrush((Color)ColorConverter.ConvertFromString(layer.correspondingMaterial().ColorCode)),
+                    Fill = Test3(),//new SolidColorBrush((Color)ColorConverter.ConvertFromString(layer.correspondingMaterial().ColorCode)),
                    // OpacityMask = GetTextureFromImage()
                 };
                 Label label = new Label()
@@ -102,142 +102,67 @@ namespace BauphysikToolWPF.UI
 
         }
 
-        /*private DrawingBrush Test()
-        {
-            DrawingBrush insulationBrush = new DrawingBrush();
-            DrawingGroup drawingGroup = new DrawingGroup();
-
-            // Create a Pen with a dashed line style and set its thickness and color
-            Pen insulationPen = new Pen(Brushes.Gray, 1);
-            insulationPen.DashStyle = new DashStyle(new double[] { 2, 2 }, 0);
-
-            // Create the geometry for the hatch pattern
-            GeometryGroup insulationGeometry = new GeometryGroup();
-
-            // Create a series of lines with 45 degree angles
-            for (int i = 0; i < 10; i++)
-            {
-                LineGeometry line1 = new LineGeometry(new Point(i * 10, 0), new Point(i * 10 + 10, 10));
-                LineGeometry line2 = new LineGeometry(new Point(i * 10, 10), new Point(i * 10 + 10, 0));
-                insulationGeometry.Children.Add(line1);
-                insulationGeometry.Children.Add(line2);
-            }
-
-            // Create a series of arcs
-            for (int i = 0; i < 10; i++)
-            {
-                ArcSegment arc = new ArcSegment(, 0, false, SweepDirection.Clockwise, true);
-                PathFigure figure = new PathFigure();
-                figure.StartPoint = new Point(i * 10, 0);
-                figure.Segments.Add(arc);
-                PathGeometry path = new PathGeometry();
-                path.Figures.Add(figure);
-                insulationGeometry.Children.Add(path);
-            }
-
-            // Create a GeometryDrawing with the created GeometryGroup
-            GeometryDrawing insulationDrawing = new GeometryDrawing(null, insulationPen, insulationGeometry);
-            drawingGroup.Children.Add(insulationDrawing);
-
-
-            // Set the Drawing property of the DrawingBrush to the DrawingGroup
-            insulationBrush.Drawing = drawingGroup;
-
-            // Set the Viewport property of the DrawingBrush to define the area that the brush will cover
-            insulationBrush.Viewport = new Rect(0, 0, 0.5, 0.5);
-
-            // Set the TileMode property of the DrawingBrush to define how the brush will be tiled across the area it covers
-            insulationBrush.TileMode = TileMode.Tile;
-
-            return insulationBrush;
-
-        }*/
-
         private DrawingBrush Test3()
         {
+
+
             DrawingBrush brush = new DrawingBrush();
 
             // Create a GeometryGroup to contain the hatch lines
-            GeometryGroup hatchContent = new GeometryGroup();
+            GeometryGroup hatchContent = new GeometryGroup();            
 
             PathGeometry pathGeometry = new PathGeometry();
 
+            //Imaginary 60x60 Rectangle
+            double arcRad = 10;
+            double arcEndX_Left = arcRad;
+            double arcEndX_Right = 60 - arcRad;
+
+            double currentY_Left = 10;
+            double currentY_Right = 0;
+
             //Imaginary Rectangle, coordinate origin is at top left corner
             PathFigure pathFigure = new PathFigure();
-            pathFigure.StartPoint = new Point(10, 0); // Startpoint of the segment series
+            pathFigure.StartPoint = new Point(arcEndX_Left, 0); // Startpoint of the segment series
             pathFigure.IsClosed = false;
             pathFigure.IsFilled = false;
 
-            ArcSegment arc = new ArcSegment();
-            arc.Point = new Point(10,20); // Connects previous Point with this (End)point of the Segment
-            arc.Size = new Size(10, 10);
-            arc.IsLargeArc = false;
-            arc.SweepDirection = SweepDirection.Counterclockwise;
-            pathFigure.Segments.Add(arc);
 
-            LineSegment line = new LineSegment() { Point = new Point(30, 11) }; // Connects previous Point with this (End)point of the Segment
-            pathFigure.Segments.Add(line);
+            for(int i = 0; i<4; i++)
+            {
+                currentY_Left += arcRad;
+                ArcSegment leftArc1 = new ArcSegment();                
+                leftArc1.Point = new Point(arcEndX_Left, currentY_Left); // Connects previous Point with this (End)point of the Segment
+                leftArc1.Size = new Size(arcRad, arcRad);
+                leftArc1.IsLargeArc = false;
+                leftArc1.SweepDirection = SweepDirection.Counterclockwise;
+                pathFigure.Segments.Add(leftArc1);
 
-            ArcSegment arc2 = new ArcSegment();
-            arc2.Point = new Point(30, 31); // Connects previous Point with this (End)point of the Segment
-            arc2.Size = new Size(10, 10);
-            arc2.IsLargeArc = false;
-            arc2.SweepDirection = SweepDirection.Clockwise;
-            pathFigure.Segments.Add(arc2);
+                currentY_Right += arcRad;
+                LineSegment connectingLineLTR = new LineSegment() { Point = new Point(arcEndX_Right, currentY_Right) }; // Connects previous Point with this (End)point of the Segment
+                pathFigure.Segments.Add(connectingLineLTR);
 
-            LineSegment line2 = new LineSegment() { Point = new Point(10, 21) }; // Connects previous Point with this (End)point of the Segment
-            pathFigure.Segments.Add(line2);
+                currentY_Left += arcRad;
+                ArcSegment rightArc1 = new ArcSegment();             
+                rightArc1.Point = new Point(arcEndX_Right, currentY_Left); // Connects previous Point with this (End)point of the Segment
+                rightArc1.Size = new Size(arcRad, arcRad);
+                rightArc1.IsLargeArc = false;
+                rightArc1.SweepDirection = SweepDirection.Clockwise;
+                pathFigure.Segments.Add(rightArc1);
 
+                currentY_Right += arcRad;
+                LineSegment connectingLineRTL = new LineSegment() { Point = new Point(arcEndX_Left, currentY_Right) }; // Connects previous Point with this (End)point of the Segment
+                pathFigure.Segments.Add(connectingLineRTL);
+            }
             pathGeometry.Figures.Add(pathFigure);
 
             hatchContent.Children.Add(pathGeometry);
+            hatchContent.Transform = new RotateTransform(45);
 
-           // This will create a half - circle with the center at(50, 10), a radius of 20, and it starts from the point(50,10) and ends at(70, 10)
-
-
-
-            // add the connecting lines
-           // hatchContent.Children.Add(new LineGeometry(new System.Windows.Point(0, 0), new System.Windows.Point(10, 10)));
-            //hatchContent.Children.Add(new LineGeometry(new System.Windows.Point(0, 10), new System.Windows.Point(10, 0)));
             // Use the hatch lines as the Drawing's content
             brush.Drawing = new GeometryDrawing(null, new Pen(Brushes.Black, 0.2), hatchContent);
 
             return brush;
-
-            // Create a new Bitmap object
-            /*Bitmap bmp = new Bitmap(100, 100);
-
-            // Create a Graphics object from the Bitmap
-            Graphics g = Graphics.FromImage(bmp);
-
-            // Create a new Pen object with the desired color and width
-            System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Brushes.Black, 2);
-
-            // Draw half circles at the top and bottom of the rectangle
-            g.DrawArc(pen, rect.Left, rect.Top, rect.Width, rect.Height / 2, 0, 180);
-            g.DrawArc(pen, rect.Left, rect.Bottom - rect.Height / 2, rect.Width, rect.Height / 2, 0, 180);
-
-            // Draw lines between the half circles to create the hatching pattern
-            g.DrawLine(pen, rect.Left, rect.Top + rect.Height / 4, rect.Right, rect.Top + rect.Height / 4);
-            g.DrawLine(pen, rect.Left, rect.Bottom - rect.Height / 4, rect.Right, rect.Bottom - rect.Height / 4);
-
-            // Draw the rectangle
-            g.DrawRectangle(new Pen(Brushes.Black, 1), rect);
-
-            /*
-                     // Create a new Pen object with the desired color and width
-        Pen pen = new Pen(Brushes.Black, 2);
-        pen.DashStyle = DashStyle.Custom;
-        pen.DashPattern = new float[] { 4, 4 };
-
-        // Draw half circles at the top and bottom of the rectangle
-        g.DrawArc(pen, 0, 0, 100, 50, 0, 180);
-        g.DrawArc(pen, 0, 50, 100, 50, 0, 180);
-
-        // Draw lines between the half circles to create the hatching pattern
-        g.DrawLine(pen, 0, 25, 100, 25);
-        g.DrawLine(pen, 0, 75, 100, 75);
-             */
         }
     }
 }
