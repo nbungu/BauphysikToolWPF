b4309bb1aecfb0da01cd7341f2551fcdd21d7050
Arne <arne.schunter@gmail.com>
2023-02-05 21:16:14 +0100

    Added UI Element prerequisites for GEG Requirement check

--- UI/ViewModels/FO0_ViewModel.cs
+++ UI/ViewModels/FO0_ViewModel.cs
@@ -8,10 +8,13 @@ namespace BauphysikToolWPF.UI.ViewModels
     {
         public string Title { get; } = "LandingPage";
         public List<Element> Elements { get; private set; }
+        public bool IsBuildingUsageA { get; set; } = false; // Usage A = Wohngebäude
+        public bool IsBuildingUsageB { get; set; } = false; // Usage B = Nicht-Wohngebäude
 
         public FO0_ViewModel() // Called by 'InitializeComponent()' from FO0_LandingPage.cs due to Class-Binding in xaml via DataContext
         {
             Elements = FO0_LandingPage.Elements;
+            // TODO Building Usage
         }
     }
 }
