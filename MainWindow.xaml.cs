6bd643c3181f2e3754395e48d3f4bf96eb7f1ac6
Arne@YOGA370 <arne.schunter@gmail.com>
2023-02-28 14:02:10 +0100

    wechsel

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using BauphysikToolWPF.UI;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace BauphysikToolWPF
@@ -12,16 +13,16 @@ namespace BauphysikToolWPF
     public enum NavigationContent 
     {
         // see in MainWindow.xaml the List of ItemsSource for indices of the ListBoxItems (Pages)
-        LandingPage = 1,
-        SetupLayer = 3,
-        SetupEnv = 4,
-        TemperatureCurve = 6,
-        GlaserCurve = 7,
-        Default = LandingPage
+        LandingPage,
+        SetupLayer,
+        SetupEnv,
+        TemperatureCurve,
+        GlaserCurve
     }
     public partial class MainWindow : Window
     {
         public static ListBox navigationMenuListBox;
+        public static ContentControl mainWindow_Content;
 
         // Saves MainWindow Instance here
         public static Window Main;
@@ -30,12 +31,39 @@ namespace BauphysikToolWPF
         {
             InitializeComponent();
             navigationMenuListBox = this.NavigationMenuListBox;
+            mainWindow_Content = this.MainWindow_Content;
             Main = this;
         }
         
         public static void SetPage(NavigationContent page)
         {
-            navigationMenuListBox.SelectedIndex = (int)page; // MainWindow.xaml Binding changes the ContentPage based on the SelectedItem string
+            // MainWindow.xaml changes the ContentPage based on the SelectedItem string
+            // The string values of the SelectedItem are defined at 'NavigationMenuItems'
+            switch (page)
+            {
+                case NavigationContent.LandingPage:
+                    mainWindow_Content.Tag = "LandingPage"; // TODO
+                    break;
+                case NavigationContent.SetupLayer:
+                    navigationMenuListBox.SelectedItem = "SetupLayer";
+                    break;
+                case NavigationContent.SetupEnv:
+                    navigationMenuListBox.SelectedItem = "SetupEnv";
+                    break;
+                case NavigationContent.TemperatureCurve:
+                    navigationMenuListBox.SelectedItem = "Temperature";
+                    break;
+                case NavigationContent.GlaserCurve:
+                    navigationMenuListBox.SelectedItem = "Moisture";
+                    break;
+                default:
+                    navigationMenuListBox.SelectedItem = "LandingPage";
+                    break;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetPage(NavigationContent.LandingPage);
         }
     }
 }
