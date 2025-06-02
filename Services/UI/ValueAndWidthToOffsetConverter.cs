eee7d54890d6ed6b2513cce87547b4bdeecd5425
Arne <arne.schunter@gmail.com>
2025-06-02 21:45:30 +0200

    Added gauge xaml control

---
+++ Services/UI/ValueAndWidthToOffsetConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BauphysikToolWPF.Services.UI
+{
+    public class ValueAndWidthToOffsetConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 4 ||
+                !(values[0] is double value) ||
+                !(values[1] is double width) ||
+                !(values[2] is double min) ||
+                !(values[3] is double max))
+                return 0.0;
+
+            double range = max - min;
+            if (range <= 0)
+                return 0.0;
+
+            double normalized = (value - min) / range;
+            double usableWidth = width - 20; // adjust margin if needed
+            return normalized * usableWidth;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ UI/CustomControls/Gauge.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BauphysikToolWPF.UI.CustomControls
+{
+    /// <summary>
+    /// Interaktionslogik für Gauge.xaml
+    /// </summary>
+    public partial class Gauge : UserControl
+    {
+        public Gauge()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+        DependencyProperty.Register(nameof(Value), typeof(double), typeof(Gauge), new PropertyMetadata(25.0));
+
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly DependencyProperty MinValueProperty =
+            DependencyProperty.Register(nameof(MinValue), typeof(double), typeof(Gauge), new PropertyMetadata(0.0));
+
+        public double MinValue
+        {
+            get => (double)GetValue(MinValueProperty);
+            set => SetValue(MinValueProperty, value);
+        }
+
+        public static readonly DependencyProperty MaxValueProperty =
+            DependencyProperty.Register(nameof(MaxValue), typeof(double), typeof(Gauge), new PropertyMetadata(100.0));
+
+        public double MaxValue
+        {
+            get => (double)GetValue(MaxValueProperty);
+            set => SetValue(MaxValueProperty, value);
+        }
+
+        public static readonly DependencyProperty ValueUnitProperty =
+            DependencyProperty.Register(nameof(ValueUnit), typeof(string), typeof(Gauge), new PropertyMetadata("m"));
+
+        public string ValueUnit
+        {
+            get => (string)GetValue(ValueUnitProperty);
+            set => SetValue(ValueUnitProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(Gauge), new PropertyMetadata("Gauge"));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty ShowReferenceMarkerTickProperty =
+            DependencyProperty.Register(nameof(ShowReferenceMarkerTick), typeof(bool), typeof(Gauge), new PropertyMetadata(true));
+
+        public bool ShowReferenceMarkerTick
+        {
+            get => (bool)GetValue(ShowReferenceMarkerTickProperty);
+            set => SetValue(ShowReferenceMarkerTickProperty, value);
+        }
+
+        public static readonly DependencyProperty ReferenceMarkerValueProperty =
+            DependencyProperty.Register(nameof(ReferenceMarkerValue), typeof(double), typeof(Gauge), new PropertyMetadata(50.0));
+
+        public double ReferenceMarkerValue
+        {
+            get => (double)GetValue(ReferenceMarkerValueProperty);
+            set => SetValue(ReferenceMarkerValueProperty, value);
+        }
+
+        public static readonly DependencyProperty ShowUnitLabelProperty =
+            DependencyProperty.Register(nameof(ShowUnitLabel), typeof(bool), typeof(Gauge), new PropertyMetadata(true));
+
+        public bool ShowUnitLabel
+        {
+            get => (bool)GetValue(ShowUnitLabelProperty);
+            set => SetValue(ShowUnitLabelProperty, value);
+        }
+    }
+}
